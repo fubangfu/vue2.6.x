b2ef56d59ae8d3bafcfceb829352fcdf1637d984
fubangfu <fubangfu@uservices.cn>
2020-07-21 17:32:22 +0800

    class调整

---
+++ src/views/form/checkbox.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-if="eq(1)">
+        <div></div>
+    </div>
+</template>
+
+<script lang="js">
+   import ExampleMixin from '../../mixins/ExampleMixin';
+
+   export default {
+      name   : 'checkbox',
+      mixins : [ExampleMixin]
+   };
+</script>
+
+<style scoped lang="scss">
+    
+
+</style>
