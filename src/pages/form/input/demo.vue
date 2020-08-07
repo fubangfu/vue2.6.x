31d22fa2beda741931b6b3d2166391a9920b5ba4
fubangfu <fubangfu@uservices.cn>
2020-08-07 14:36:51 +0800

    补充文档

--- src/pages/form/input/demo.vue
+++ src/pages/form/input/demo.vue
@@ -17,23 +17,6 @@
 
 import AqireInput from '../../../packages/input/AqireInput';
 import Example from '../../../components/Example';
-import validator from '@aqire/validator';
-import { integer, maxlength, number } from '@aqire/validator/build/validates';
-
-
-const v = validator(
-    number( '必须是数字' ),
-    maxlength( 12, '不能超过12位' ),
-    integer( '必须是整数' ),
-    () => new Promise( ( resolve, reject ) => reject( new Error( '不能通过' ) ) )
-);
-
-v.runAsync( '1234567890.' ).then( () => {
-    console.log( true );
-} ).catch( err => {
-    console.error( err );
-} );
-
 
 export default {
     name       : 'demo',
